dbe107c476b3981fb57bb4b66416749d6b31f062
dbrao <75373300+dbrao@users.noreply.github.com>
2021-07-01 12:37:05 +0530

    first commit..

---
+++ src/sample_testng_project/test1.java
@@ -0,0 +1,21 @@
+package sample_testng_project;
+
+import org.testng.annotations.Test;
+
+public class test1 {
+	
+	@Test
+	
+	public void show() {
+		
+		System.out.println("Hello World");
+	}
+	
+	@Test
+	
+	public void display() {
+		
+		System.out.println("Welcome to Selenium");
+	}
+
+}
---
+++ src/sample_testng_project/test2.java
@@ -0,0 +1,21 @@
+package sample_testng_project;
+
+import org.testng.annotations.Test;
+
+public class test2 {
+
+	@Test
+
+	public void show() {
+
+		System.out.println("Hello World");
+	}
+
+	@Test
+
+	public void display() {
+
+		System.out.println("Welcome to Selenium");
+	}
+
+}
